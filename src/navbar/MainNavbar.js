04aaddcdda99552a9e80c3137d304a91ae61f44a
seulgiwendy <wheejuni@gmail.com>
2018-04-11 18:41:20 +0900

    [navbar]색깔을 검은색으로 수정

--- src/navbar/MainNavbar.js
+++ src/navbar/MainNavbar.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Image } from 'react-bootstrap';
 import './navbar.css';
 
 
 const MainNavbar = (props) => {
     return(
-        <Navbar className="main-navbar">
+        <Navbar className="main-navbar container-fluid">
             <Navbar.Header>
                 <Navbar.Brand>
-                    <span className="navbar-startupall">스타트업 얼라이언스</span>
+                    <Image src="http://startupall.kr/wp/wp-content/uploads/2017/01/favicon-32x32.png"/>
                 </Navbar.Brand>
             </Navbar.Header>
+            <Nav>
+                <NavItem>My Page</NavItem>
+                <NavItem>문의하기</NavItem>
+                <NavItem>startupall.kr</NavItem>
+            </Nav>
         </Navbar>
     )
 }
