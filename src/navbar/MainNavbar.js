e4b11989804136601d564681fca0a37b2178706a
seulgiwendy <wheejuni@gmail.com>
2018-04-11 18:25:13 +0900

    [navbar]브랜드 폰트 색상 변경

--- src/navbar/MainNavbar.js
+++ src/navbar/MainNavbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import './navbar.css';
 
 
 const MainNavbar = (props) => {
     return(
-        <Navbar>
+        <Navbar className="main-navbar">
             <Navbar.Header>
                 <Navbar.Brand>
-                    스타트업 얼라이언스
+                    <span className="navbar-startupall">스타트업 얼라이언스</span>
                 </Navbar.Brand>
             </Navbar.Header>
         </Navbar>
