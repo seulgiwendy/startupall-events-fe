ad5e44122ec20accbb0209b5430e129a39d869d4
seulgiwendy <wheejuni@gmail.com>
2018-04-11 19:17:39 +0900

    [navbar]유저정보 버튼 추가

--- src/navbar/MainNavbar.js
+++ src/navbar/MainNavbar.js
@@ -5,17 +5,25 @@ import './navbar.css';
 
 const MainNavbar = (props) => {
     return(
-        <Navbar className="main-navbar container-fluid">
+        <Navbar className="main-navbar">
             <Navbar.Header>
                 <Navbar.Brand>
                     <Image src="http://startupall.kr/wp/wp-content/uploads/2017/01/favicon-32x32.png"/>
                 </Navbar.Brand>
             </Navbar.Header>
-            <Nav>
-                <NavItem>My Page</NavItem>
-                <NavItem>문의하기</NavItem>
-                <NavItem>startupall.kr</NavItem>
-            </Nav>
+                <Nav className="center-navbar">
+                    <NavItem id="navitem">My Page</NavItem>
+                    <NavItem id="navitem">문의하기</NavItem>
+                    <NavItem id="navitem">startupall.kr</NavItem>
+                </Nav>
+                <Nav className="navbar-useravatar" pullRight>
+                    <div className="btn btn-default navbar-userbtn">
+                    <div className="navbar-user-img">
+                        <Image src="https://www.famousbirthdays.com/faces/seulgi-image.jpg" circle/>
+                        <span id="username">seulgiwendy님</span>
+                    </div>
+                    </div>
+                </Nav>
         </Navbar>
     )
 }
