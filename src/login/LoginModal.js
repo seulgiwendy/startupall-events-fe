7515d2a823c50e153d1c49c1e1144de17a4bff88
seulgiwendy <wheejuni@gmail.com>
2018-04-14 20:46:50 +0900

    [login]로그인 모달 작성

---
+++ src/login/LoginModal.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Modal, FormGroup, FormControl } from 'react-bootstrap';
+import FacebookLogin from 'react-facebook-login';
+import './login.css';
+
+class LoginModal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.userLoggedIn = this.userLoggedIn.bind(this);
+        this.userLoggedOut = this.userLoggedOut.bind(this);
+     
+    }
+
+    componentDidMount() {
+        
+    }
+
+    userLoggedIn(response) {
+        console.log(response);
+    }
+
+    userLoggedOut() {
+        console.log('fuck! logout!');
+    }
+    render() {
+        return (
+            <Modal show={this.props.showModal} onHide={this.props.hideModal} onEntered={this.modalShow}dialogClassName="login-modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>로그인</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="loginmodal-body container-fluid">
+                    
+                    <div className="row">
+                    <div className="loginmodal-form col-md-6">
+                    <h4 className="text-center">회원 로그인</h4>
+                        <form>
+                            <FormGroup>
+                                <FormControl className="loginmodal-box loginmodal-email" id="loginEmail" type="email" placeholder="로그인 이메일"/>   
+                                <FormControl className="loginmodal-box loginmodal-password" id="loginPassword" type="password" placeholder="비밀번호(8자 이상)"/>
+                            </FormGroup>
+                            <a className="loginmodal-box loginmodal-iforgot">계정을 잃어버리셨나요?</a>
+                            
+                        </form>
+                        <div className="loginmodal-form-buttons">
+                            <button className="btn btn-primary login-button"><span className="login-icons fas fa-sign-in-alt"/>로그인</button>
+                            <button className="btn btn-default login-button"><span className="login-icons fas fa-user-plus"/>회원 가입</button>
+                        </div>
+                    </div>
+                    <div className="loginmodal-social col-md-6">
+                        <h4 className="text-center">소셜 로그인</h4>
+                        <div className="social-form-textbox">
+                            소셜 계정으로 스타트업 얼라이언스의 행사를 편리하게 예약하세요!<br/>
+                            (처음 한 번은 회원가입이 필요합니다.)
+                        </div>
+                        <button className="social-button kakao-button"></button>
+                        <button className="social-button naver-button"></button>
+                        <button className="social-button btn btn-block btn-social btn-facebook"><span className="fab fa-facebook"></span>Facebook 로그인</button>
+                    </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        )
+    } 
+}
+
+export default LoginModal;
--- src/navbar/MainNavbar.js
+++ src/navbar/MainNavbar.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, Image } from 'react-bootstrap';
 import './navbar.css';
+import LoginModal from '../login/LoginModal';
 
 
-const MainNavbar = (props) => {
-    return(
-        <Navbar className="main-navbar">
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <Image src="http://startupall.kr/wp/wp-content/uploads/2017/01/favicon-32x32.png"/>
-                </Navbar.Brand>
-            </Navbar.Header>
-                <Nav className="center-navbar">
-                    <NavItem id="navitem">My Page</NavItem>
-                    <NavItem id="navitem">문의하기</NavItem>
-                    <NavItem id="navitem">startupall.kr</NavItem>
-                </Nav>
-                <Nav className="navbar-useravatar" pullRight>
-                    <div className="btn btn-default navbar-userbtn">
-                    <div className="navbar-user-img">
-                        <Image src="https://www.famousbirthdays.com/faces/seulgi-image.jpg" circle/>
-                        <span id="username">seulgiwendy님</span>
-                    </div>
-                    </div>
-                </Nav>
-        </Navbar>
-    )
+
+
+class MainNavbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginExpanded : false
+        }
+
+        this.handleLoginButtonOnclick = this.handleLoginButtonOnclick.bind(this);
+        this.handleLoginModalDismiss = this.handleLoginModalDismiss.bind(this);
+    }
+
+    handleLoginButtonOnclick() {
+        this.setState({
+            loginExpanded: true
+        })
+    }
+
+    handleLoginModalDismiss() {
+        this.setState({
+            loginExpanded: false
+        });
+    }
+
+    render() {
+        console.log(this.state.loginExpanded);
+
+        return(
+            <Navbar className="main-navbar">
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Image src="http://startupall.kr/wp/wp-content/uploads/2017/01/favicon-32x32.png"/>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                    <Nav className="center-navbar">
+                        <NavItem id="navitem">My Page</NavItem>
+                        <NavItem id="navitem">문의하기</NavItem>
+                        <NavItem id="navitem">startupall.kr</NavItem>
+                    </Nav>
+                    <Nav className="navbar-useravatar" pullRight>
+                        <button className="btn btn-default navbar-userbtn" onClick={this.handleLoginButtonOnclick}>
+                        <div className="navbar-user-img">
+                            <Image src="https://www.famousbirthdays.com/faces/seulgi-image.jpg" circle/>
+                            <span id="username">seulgiwendy님</span>
+                        </div>
+                        </button>
+                    </Nav>
+                    <LoginModal showModal={this.state.loginExpanded} hideModal={this.handleLoginModalDismiss}/>
+            </Navbar>
+        )
+    }
 }
 
 export default MainNavbar;
